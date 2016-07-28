743afa6eabe64f037aa3ab502a08e7a979d73685
Alvaro <alvarofleith@gmail.com>
2016-07-28 19:41:33 -0300

    fix ESLINT errors and add steps to README

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,15 +1,15 @@
+'use strict';
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
-  router.post('/create/model', function(req, res)
-			{
-			    var db = server.dataSources.db;
-			    var new_model = req.body;
-			    var model = db.createModel(new_model["name"], new_model["fields"]);
-			    server.model(model);
-			    res.send('Model created!');
-			}
-  	);
+  router.post('/create/model', function(req, res) {
+    var db = server.dataSources.db;
+    var newModel = req.body;
+    var model = db.createModel(newModel['name'], newModel['fields']);
+    server.model(model);
+    res.send('Model created!');
+  });
   server.use(router);
 };
