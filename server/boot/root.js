f68cdccc9e30564dcb9197257ce20db3d3527528
Alvaro <alvarofleith@gmail.com>
2016-07-28 19:11:36 -0300

    scaffold generated by yeoman and add an api to create a new model

---
+++ server/boot/root.js
@@ -0,0 +1,15 @@
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  router.get('/', server.loopback.status());
+  router.post('/createModel', function(req, res)
+			{
+			    var db = server.dataSources.db;
+			    var new_model = req.body;
+			    var model = db.createModel(new_model["name"], new_model["fields"]);
+			    server.model(model);
+			    res.send('Model created!');
+			}
+  	);
+  server.use(router);
+};
